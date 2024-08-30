816e1ec833977d81f401e989155e1536e74ab021
MrSakine <msakine20@gmail.com>
2024-08-30 21:15:49 +0000

    default port update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json())
 app.use("/", routes);
 
-const PORT = process.env.PORT || 1234;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
